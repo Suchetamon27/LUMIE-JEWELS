124fc63a30752608595548c2eec95e050e4063b5
Suchetamon27 <suchetamonbol@gmail.com>
2026-07-10 13:57:33 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
+import tailwindAnimate from 'tailwindcss-animate';
+import containerQuery from '@tailwindcss/container-queries';
+import intersect from 'tailwindcss-intersect';
 
+export default {
+    darkMode: ['class'],
+    content: [
+        './index.html',
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+…        },
+    ],
+};
